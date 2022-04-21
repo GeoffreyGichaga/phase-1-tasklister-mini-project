36c3c70d012f862a67ad36edc290ff871e92f496
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-04-21 04:00:43 +0300

    completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (e) => {
   // your code here
+  e.preventDefault()
+
+  let subBtn = document.getElementById('addTask')
+  let toDo = document.getElementById("tasks")
+
+  subBtn.addEventListener("click",addItems)
+
+  
+
+
+
 });
+
+let addItems = (e)=>{
+  e.preventDefault()
+  let taskDesc = document.getElementById("new-task-description").value  
+  let appendList = document.querySelector("#tasks")
+  let listItem = `${taskDesc}`
+
+  let createList = document.createElement('li')
+  createList.textContent = listItem
+  appendList.append(createList)
+
+
+
+
+
+
+
+}
+
